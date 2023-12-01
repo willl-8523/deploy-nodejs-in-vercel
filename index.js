38decb0c5350943becbbe7a104f9b797dcaef5f4
willl85 <ngowilfried@gmail.com>
2023-12-01 12:16:09 +0100

    Read file in data folder

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-
+const fs = require('node:fs/promises');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const PORT = 4000;
 
-app.get('/home', (req, res) => {
-  res.status(200).json('Welcome, your app is working well');
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.get('/home', async (req, res) => {
+  const data = await fs.readFile('data/events.json', 'utf8');
+  res.status(200).json(data);
 });
 
 app.listen(PORT, () => {
@@ -13,3 +23,6 @@ app.listen(PORT, () => {
 
 // Export the Express API
 module.exports = app;
+
+
+
